98cbd47a847ee665db8bfbb6cf13029e341e76a7
Diego Moreira <chicodiegomoreira@gmail.com>
2020-06-28 13:19:45 -0300

    First Commit: Setup

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+public class CameraController : SingletonMonoBehaviour<CameraController> {
+    
+    public int cameraWidth;
+
+    void Start() {
+        Miscellaneous.SetCameraOrthographicSizeByWidth(
+            GetComponent<Camera>(),
+            cameraWidth
+        );
+    }
+}

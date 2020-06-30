a3644069b3849fc4078bf260c00af2361eabeef0
Diego Moreira <chicodiegomoreira@gmail.com>
2020-06-30 14:17:01 -0300

    Feature: UI Filter Type Item, Fonts

---
+++ Assets/Scripts/UI/TToM_Dropdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class TToM_Dropdown : TMP_Dropdown {
+    RectTransform rt_DropdownList;
+    RectTransform rt_DropdownItem;
+    protected override GameObject CreateDropdownList(GameObject template) {
+        GameObject currentDropdownList = base.CreateDropdownList(template);
+        rt_DropdownList = currentDropdownList.GetComponent<RectTransform>();
+        rt_DropdownItem = currentDropdownList.GetComponentInChildren<DropdownItem>()
+                            .rectTransform;
+        
+        return currentDropdownList;
+    }
+
+    public override void OnPointerClick(PointerEventData eventData) {
+        Show();
+        
+        rt_DropdownList.sizeDelta = new Vector2(
+                0, rt_DropdownItem.sizeDelta.y * (options.Count + 1)
+            );
+
+        captionText.GetComponent<Canvas>().sortingOrder =
+            rt_DropdownList.GetComponent<Canvas>().sortingOrder;
+    }
+}

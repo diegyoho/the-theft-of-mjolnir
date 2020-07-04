411128625884aad555b512dac346759cf5cd41b7
Diego Moreira <chicodiegomoreira@gmail.com>
2020-07-03 22:20:04 -0300

    Feature: BG Video

--- Assets/Scripts/Controllers/BGVideoController.cs
+++ Assets/Scripts/Controllers/BGVideoController.cs
@@ -6,58 +6,49 @@ using UnityEngine.Video;
 [RequireComponent(typeof(AudioSource))]
 public class BGVideoController : MonoBehaviour {
     
-    VideoPlayer[] videoPlayer = new VideoPlayer[2];
+    VideoPlayer videoPlayer;
     
     void Start() {
-        videoPlayer[0] =  new GameObject("Video Player 1").AddComponent<VideoPlayer>();
-        videoPlayer[0].playOnAwake = false;
-        videoPlayer[0].renderMode = VideoRenderMode.CameraFarPlane;
-        videoPlayer[0].targetCamera = Camera.main;
-        videoPlayer[0].audioOutputMode = VideoAudioOutputMode.AudioSource;
-        videoPlayer[0].SetTargetAudioSource(0, GetComponent<AudioSource>());
-
-        videoPlayer[1] =  new GameObject("Video Player 2").AddComponent<VideoPlayer>();
-        videoPlayer[1].playOnAwake = false;
-        videoPlayer[1].renderMode = VideoRenderMode.CameraFarPlane;
-        videoPlayer[1].targetCamera = Camera.main;
-        videoPlayer[1].audioOutputMode = VideoAudioOutputMode.AudioSource;
-        videoPlayer[1].SetTargetAudioSource(0, GetComponent<AudioSource>());
-        
-        videoPlayer[0].loopPointReached += EndReached;
-
+        videoPlayer =  new GameObject("Video Player 1").AddComponent<VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        videoPlayer.renderMode = VideoRenderMode.CameraFarPlane;
+        videoPlayer.targetCamera = Camera.main;
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.SetTargetAudioSource(0, GetComponent<AudioSource>());
+        videoPlayer.waitForFirstFrame = false;
+        videoPlayer.loopPointReached += EndReached;
         StartCoroutine(PlayVideo());
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space))
+            EndReached(videoPlayer);
     }
 
     IEnumerator PlayVideo() {
         
-        videoPlayer[0].url = System.IO.Path.Combine (Application.streamingAssetsPath,"bg-intro.mp4");
-
-        videoPlayer[1].url = System.IO.Path.Combine (Application.streamingAssetsPath,"bg-loop.mp4");
-        videoPlayer[1].isLooping = true;
+        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"bg-ttom.mp4");
         
-        videoPlayer[0].Prepare();
-        videoPlayer[1].Prepare();
+        videoPlayer.Prepare();
         
-        yield return new WaitUntil(() => videoPlayer[0].isPrepared);
+        yield return new WaitUntil(() => videoPlayer.isPrepared);
 
         yield return new WaitForSeconds(1f);
 
-        videoPlayer[0].Play();
+        videoPlayer.Play();
 
-        yield return new WaitUntil(() => !videoPlayer[0].isPlaying);
-        
-        yield return new WaitUntil(() => videoPlayer[1].isPrepared);
-
-        Destroy(videoPlayer[0].gameObject);
-        
-        videoPlayer[1].Play();
-
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(2.531f);
 
         MainUIController.ShowUI();
     }
 
-    void EndReached(VideoPlayer vp) {
-        vp.Pause();
+    void EndReached(VideoPlayer videoPlayer) {
+        videoPlayer.started += Jump;
+        videoPlayer.Play();
+    }
+
+    void Jump(VideoPlayer videoPlayer) {
+        videoPlayer.time = 2.531f;
+        videoPlayer.started -= Jump;
     }
 }
